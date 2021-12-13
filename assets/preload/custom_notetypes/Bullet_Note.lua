0048a9bc29631f82ef4e091a64be6580e9eac062
Cauasobre51 <96015002+Cauasobre51@users.noreply.github.com>
2021-12-13 18:47:06 -0300

    Add files via upload

--- assets/preload/custom_notetypes/Bullet_Note.lua
+++ assets/preload/custom_notetypes/Bullet_Note.lua
@@ -2,7 +2,7 @@ function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
 		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Bullet_Note' then --Check if the note on the chart is a Bullet Note
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'assets/assets/shared/images/Bullet_Note'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'shared/images/Bullet_NOTE_assets'); --Change texture
 			setPropertyFromGroup('unspawnNotes', i, 'noteSplashHue', 0); --custom notesplash color, why not
 			setPropertyFromGroup('unspawnNotes', i, 'noteSplashSat', -20);
 			setPropertyFromGroup('unspawnNotes', i, 'noteSplashBrt', 1);
